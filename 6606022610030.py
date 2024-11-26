eccefbf47ff99e70a7974ef0af5b05ac55b5c9e8
Shinnamon-Roll <chimonkung0@gmail.com>
2024-11-26 15:03:52 +0700

    pama

---
+++ 6606022610030.py
@@ -0,0 +1,15 @@
+#print(f"Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamod in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are!")
+
+line = "-" * 72
+
+print(f"{'Name':>15}{'Shares':>15}{'Price':>15}{'Change':>15}")
+print(f"{line:>15}")
+
+print(f"{'AA':>15}{100:>15d}{'$':>1}{9.22:>14,.2f}{'-22.98':>15}")
+print(f"{'IBM':>15}{50:>15d}{'$':>1}{106.25:>14,.2f}{'15.18':>15}")
+print(f"{'CAT':>15}{150:>15d}{'$':>1}{35.46:>14,.2f}{'-47.98':>15}")
+print(f"{'MSFT':>15}{200:>15d}{'$':>1}{20.89:>14,.2f}{'-30.34':>15}")
+print(f"{'CAT':>15}{150:>15d}{'$':>1}{35.46:>14,.2f}{'-47.98':>15}")
+print(f"{'GE':>15}{95:>15d}{'$':>1}{13.48:>14,.2f}{'-44.21':>15}")
+print(f"{'MSFT':>15}{50:>15d}{'$':>1}{20.89:>14,.2f}{'-44.21':>15}")
+print(f"{'IBM':>15}{100:>15d}{'$':>1}{106.28:>14,.2f}{'35.84':>15}")
